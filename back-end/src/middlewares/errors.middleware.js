a23a22287be2a642e2974a4d058fe14c89045872
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-27 18:07:29 -0300

    chore: Adding Joi dependencies to packages and adding a commented example to errors middleware

--- back-end/src/middlewares/errors.middleware.js
+++ back-end/src/middlewares/errors.middleware.js
@@ -1,4 +1,10 @@
 const errorMiddleware = (err, _req, res, _next) => {
+
+  // Cond example
+  // if (err.name === 'JsonWebTokenError') {
+  //   return res.status(401).json({ message: 'Expired or invalid token' });
+  // }
+
   res.status(500).json({ message: err.message });
 }
 
