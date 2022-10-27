019d93e1217d3ce006646aaae8422c173f23caeb
jvdionizio <dionizio145@gmail.com>
2022-10-27 17:09:03 -0300

    Configurando Router e Context API

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Router, Routes } from 'react-router-dom';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Login /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
