c4b162fb44468a17c99b783095fa09bf19123594
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-28 18:03:50 -0300

    feat: Adding route to /seller/orders

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Orders from './pages/Orders';
 import OrdersDetails from './pages/OrdersDetails';
+import OrdersBySeller from './pages/OrdersBySeller';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
           <Route exact path="/" element={ <Login /> } />
           <Route exact path="customer/orders" element={ <Orders /> } />
           <Route exact path="customer/orders/:id" element={ <OrdersDetails /> } />
+          <Route exact path="seller/orders" element={ <OrdersBySeller /> } />
+          <Route exact path="seller/orders/:id" element={ <OrdersDetails /> } />
         </Routes>
       </div>
     </Router>
