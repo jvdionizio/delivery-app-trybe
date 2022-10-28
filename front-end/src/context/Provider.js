866f4db5e659c2925b5b5497e0d5aa6493d56f89
Giuseppe <giuseppeuhlmann@gmail.com>
2022-10-28 19:58:31 -0300

    feat: create cart total price and local storage cart items

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -1,13 +1,30 @@
 import PropTypes from 'prop-types';
+import { useState, useMemo } from 'react';
 import Context from './Context';
 
 function Provider({ children }) {
+  const [users, setUsers] = useState({
+    message: 'certo',
+  });
+
+  const [totalPrice, setTotalPrice] = useState('0,00');
+
+  const value = useMemo(() => ({
+    users,
+    setUsers,
+    totalPrice,
+    setTotalPrice,
+  }), [users, totalPrice]);
+
   return (
-    <Context.Provider>
+    <Context.Provider
+      value={ value }
+    >
       {children}
     </Context.Provider>
   );
 }
+
 Provider.propTypes = {
   children: PropTypes.node.isRequired,
 };
--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header';
 import ProductCard from '../components/ProductCard';
+import Context from '../context/Context';
 
 function CustomerProducts() {
+  const { totalPrice } = useContext(Context);
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState({});
@@ -14,24 +16,23 @@ function CustomerProducts() {
 
   const fetchProducts = async () => {
     const { data } = await axios.get('http://localhost:3001/products');
-    console.log(data);
 
     setLoading(false);
     setProducts(data);
   };
 
   const verifyLogin = async () => {
-    const { token } = JSON.parse(localStorage.getItem('user')) || '';
-    // const data = JSON.parse(localStorage.getItem('user')) || ''; // mock
-    // fetchProducts();
-    // setUser(data);
+    const data = JSON.parse(localStorage.getItem('user')) || ''; // mock
+    fetchProducts();
+    setUser(data);
+    // const { token } = JSON.parse(localStorage.getItem('user')) || '';
 
-    await axios.get('http://localhost:3001/token', { token })
-      .then((response) => {
-        fetchProducts();
-        setUser(response.data);
-      })
-      .catch(() => navigate('/login'));
+    // await axios.get('http://localhost:3001/token', { token })
+    //   .then((response) => {
+    //     fetchProducts();
+    //     setUser(response.data);
+    //   })
+    //   .catch(() => navigate('/login'));
   };
 
   useEffect(() => {
@@ -49,10 +50,18 @@ function CustomerProducts() {
             name={ product.name }
             price={ product.price }
             image={ product.urlImage }
-            id={ index }
+            id={ product.id }
+            dataTestId={ index }
           />
         ))
       }
+      <button
+        type="button"
+        data-testid="customer_products__checkout-bottom-value"
+        onClick={ () => navigate('/customer/checkout') }
+      >
+        {`Ver carrinho: R$ ${totalPrice}`}
+      </button>
     </div>
   );
 }
