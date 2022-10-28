27b8b297ee2434a0ddd3213918da9dbd835f9ecd
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-28 14:10:17 -0300

    feat: controller for orders

--- back-end/src/controllers/salesController.js
+++ back-end/src/controllers/salesController.js
@@ -1,12 +1,59 @@
 const salesService = require('../services/salesService');
 
 const getAllByUserId = async (req, res) => {
-  const { userId } = req.body;
+  const { id } = req.params;
 
-  const sales = await salesService.getAllByUserId(userId);
+  const sales = await salesService.getAllByUserId(id);
+  console.log(sales);
+
+  if (sales.length === 0) {
+    throw new Error('404|Cannot find any sale');
+  }
+
   res.status(200).json(sales);
+};
+
+const getAllById = async (req, res) => {
+  const { id } = req.params;
+
+  const sales = await salesService.getAllBySaleId(id);
+
+  if (!sales) {
+    throw new Error('404|Cannot find any sale');
+  }
+
+  res.status(200).json(sales);
+};
+
+const updateStatusBySaleId = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  const sale = await salesService.getByPk(id);
+  if (!sale) {
+    throw new Error('404|Cannot find any sale');
+  }
+
+  await salesService.updateStatusBySaleId(id, status);
+  
+  res.status(200).json({ message: `Sale ${id}: Status updated to ${status}` });
+};
+
+const getSellerById = async (req, res) => {
+  const { id } = req.params;
+
+  const seller = await salesService.getSellerById(id);
+
+  if (!seller) {
+    throw new Error('404|Cannot find any seller');
+  }
+
+  res.status(200).json(seller);
 };
 
 module.exports = {
   getAllByUserId,
+  getAllById,
+  updateStatusBySaleId,
+  getSellerById,
 };
