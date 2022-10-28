112155c6a39fddfcdffa92926ad7c5221910b04f
jvdionizio <dionizio145@gmail.com>
2022-10-28 16:35:15 -0300

    Feat: Login and Register forms with verified fields

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
+          <Route exact path="/login" element={ <Login /> } />
+          <Route exact path="/register" element={ <Register /> } />
           <Route exact path="/" element={ <Login /> } />
+          <Route exact path="/*" element={ <NotFound /> } />
         </Routes>
       </div>
     </Router>
