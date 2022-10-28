c9c40800884a191204d020037c302837f59d7ff3
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-28 15:39:19 -0300

    feat: Implement seller orders route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -11,7 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/test', routes.testRoute);
-app.use('/customer/orders', routes.salesRoute);
+app.use('/customer/orders', routes.salesCustomerRoute);
+app.use('/seller/orders', routes.salesSellerRoute);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(errorMiddleware);
