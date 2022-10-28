66df4c55831db0f02b662675658130995cb1d74e
vtorvieira <vieira.vitor42@gmail.com>
2022-10-28 18:41:39 -0300

    fix: fix some lint

--- back-end/src/services/checkoutService.js
+++ back-end/src/services/checkoutService.js
@@ -8,13 +8,20 @@ const sequelize = new Sequelize(
 
 const CheckoutServices = {
   addSale: async (body) => {
-    const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, products } = body;
-    console.log(products);
+    const {
+      userId,
+      sellerId,
+      totalPrice,
+      deliveryAddress,
+      deliveryNumber,
+      status,
+      products,
+    } = body;
 
     const result = await sequelize.transaction(async (t) => {
       const { id } = await Sales.create({
         userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, saleDate: Date(), status,
-      }, { transaction: t } );
+      }, { transaction: t });
   
       await SalesProducts.bulkCreate(
         products.map((prod) => ({
