fab8bfc892c1b3f5ca285b43c970271efaa2f9ab
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-28 14:00:35 -0300

    feat: Adding methods to get products and seller info

--- back-end/src/services/salesService.js
+++ back-end/src/services/salesService.js
@@ -1,7 +1,45 @@
-const { Sales } = require('../database/models');
+const { Sales, SalesProducts, Products, Users } = require('../database/models');
 
 const getAllByUserId = async (userId) => Sales.findAll({ where: { userId } });
 
+const getAllBySaleId = async (saleId) => {
+  const sale = await Sales.findByPk(saleId , {
+    include: [{
+        model: Products,
+        as: 'salesProducts',
+        through: {
+          model: SalesProducts,
+          as: 'productsSales',
+          attributes: ['quantity'],
+        },
+      }],
+    attributes: { exclude: ['deliveryAddress', 'deliveryNumber'] },
+  });
+
+  return sale;
+};
+
+const getSellerById = async (sellerId) => {
+  const test = await Users.findByPk(sellerId , {
+      attributes: { exclude: ['id', 'email', 'password', 'role'] },
+  });
+  console.log(sellerId);
+  console.log(test);
+  return test;
+};
+
+const getByPk = async (id) => Sales.findByPk(id);
+
+const updateStatusBySaleId = async (saleId, status) => {
+  const sale = await Sales.findByPk(saleId);
+  sale.status = status;
+  await sale.save();
+};
+
 module.exports = {
   getAllByUserId,
+  getAllBySaleId,
+  getByPk,
+  updateStatusBySaleId,
+  getSellerById,
 };
