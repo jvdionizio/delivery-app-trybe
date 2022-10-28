8aa44afb910fade0047cfc703621d0f3d68332ed
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-28 14:20:51 -0300

    feat: Implement routes to pages orders and orders details

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Orders from './pages/Orders';
+import OrdersDetails from './pages/OrdersDetails';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={ <Login /> } />
+          <Route exact path="customer/orders" element={ <Orders /> } />
+          <Route exact path="customer/orders/:id" element={ <OrdersDetails /> } />
         </Routes>
       </div>
     </Router>
