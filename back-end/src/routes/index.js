f30180c6bd5063d63034cea20634a992dab3d4f5
lfpborba <lfpborba@icloud.com>
2022-10-30 15:24:32 -0300

    feat: implementing login route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,7 +1,9 @@
 const testRoute = require('./example');
 const salesRoute = require('./sales');
+const userRoute = require('./login');
 
 module.exports = {
   testRoute,
   salesRoute,
+  userRoute,
 };
