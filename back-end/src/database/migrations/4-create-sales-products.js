69c5f2fba5110308702e51016a195c4bbbf527bc
giuseppeusn <giuseppeuhlmann@gmail.com>
2022-10-31 18:01:01 -0300

    fix: wrong model name

--- back-end/src/database/migrations/4-create-sales-products.js
+++ back-end/src/database/migrations/4-create-sales-products.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references:{
-          model: 'Products',
+          model: 'products',
           key: 'id',
         },
         primaryKey: true,
