8ad157433daeff6981380be6eaa1e03210e20190
vitorbss12 <vitor_bss10@hotmail.com>
2022-11-01 17:15:14 -0300

    feat: Validating if user is logged and have a valid token

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import OrdersList from '../components/OrdersList';
+import Header from '../components/Header';
+import { verifyToken } from '../services/requests';
 
-function Login() {
-  // <Header />
+function Orders() {
+  const [user, setUser] = useState(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const currentUser = JSON.parse(localStorage.getItem('user') || {});
+      const logged = await verifyToken(currentUser?.token);
+      if (logged) {
+        setUser(currentUser);
+      } else {
+        navigate('/login');
+      }
+    })();
+  }, [navigate]);
+
   return (
-    <OrdersList client="customer" />
+    <div>
+      { user ? (
+        <div>
+          <Header user={ user.name } />
+          <OrdersList client="customer" userId={ user.id } />
+        </div>
+      ) : <p>Loading...</p> }
+    </div>
   );
 }
 
-export default Login;
+export default Orders;
