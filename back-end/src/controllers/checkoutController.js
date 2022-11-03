13132a25312717bd39ef3f5e3eba56877bb2f730
vtorvieira <vieira.vitor42@gmail.com>
2022-11-03 10:25:29 -0300

    fix: fix lint

--- back-end/src/controllers/checkoutController.js
+++ back-end/src/controllers/checkoutController.js
@@ -3,7 +3,7 @@ const CheckoutServices = require('../services/checkoutService');
 
 const CheckoutController = {
   addSale: async (req, res) => {
-    verifyToken(req.headers.authorization)
+    verifyToken(req.headers.authorization);
     const id = await CheckoutServices.addSale(req.body);
     return res.status(201).json(id);
   },
