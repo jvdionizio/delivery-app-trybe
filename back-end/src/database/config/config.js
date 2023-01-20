264e5b0b373a289abbc72f944e3cf1e39523bbd0
jvdionizio <dionizio145@gmail.com>
2023-01-20 16:00:01 -0300

    fix: env variables

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,20 +1,11 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQLHOST || 'containers-us-west-195.railway.app',
+  host: process.env.MYSQLHOST || 'containers-us-west-195.railway.app',
   port: process.env.MYSQLPORT || '5604',
   database: 
-    `${process.env.MYSQLDBNAME || 'railway'}${suffix[environment] || suffix.test}`,
+    `${process.env.MYSQLDBNAME || 'railway'}`,
   username: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || '4gWGKibbu8mE3bFmc7Yw',
   dialect: 'mysql',
