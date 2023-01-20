6c53a6a3c95bd09066bb637bf5f6cb22deff3e51
jvdionizio <dionizio145@gmail.com>
2023-01-20 15:38:27 -0300

    fix: url for request

--- front-end/src/services/requests.js
+++ front-end/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://delivery-app-trybe-production.up.railway.app/}',
+  baseURL: 'https://delivery-app-trybe-production.up.railway.app',
 });
 
 export const setToken = (token) => {
